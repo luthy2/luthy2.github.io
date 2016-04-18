e8ef295c1c8d9e1d45fc67ed8edf5729662d1e29
Justin Luthy <Justin@Justins-MacBook-Pro.local>
2016-04-18 15:57:52 -0700

    fonts, colors, links

--- list.js
+++
@@ -1,9 +0,0 @@
-angular.module('listApp',[])
-  .controller('ListController', function(){
-    var list = this;
-    list.items = [];
-    list.addItem = function(){
-      list.items.push(list.itemText)
-      list.itemText = ''
-    };
-  });
